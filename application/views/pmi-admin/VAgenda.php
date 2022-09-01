8502ea7b3f3cc51b69245de27625f670bae1eff5
Dedy Hermawan <48717251+DedyHrmawan@users.noreply.github.com>
2022-09-01 15:52:09 +0700

    fix button logout

--- application/views/pmi-admin/VAgenda.php
+++ application/views/pmi-admin/VAgenda.php
@@ -1,41 +1,41 @@
 <body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed aside-enabled aside-fixed">
-		<!--begin::Main-->
-		<!--begin::Root-->
-		<div class="d-flex flex-column flex-root">
-			<!--begin::Page-->
-			<div class="page d-flex flex-row flex-column-fluid">
-				<!--begin::Aside-->
-				<div id="kt_aside" class="aside aside-dark aside-hoverable" data-kt-drawer="true" data-kt-drawer-name="aside" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_aside_mobile_toggle">
-					<!--begin::Brand-->
-					<div class="aside-logo flex-column-auto" id="kt_aside_logo">
-						<!--begin::Logo-->
-						<a href="../../demo1/dist/index.html">
-							<img alt="Logo" src="assets/media/logos/logo-1-dark.svg" class="h-25px logo" />
-						</a>
-						<!--end::Logo-->
-						<!--begin::Aside toggler-->
-						<div id="kt_aside_toggle" class="btn btn-icon w-auto px-0 btn-active-color-primary aside-toggle" data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body" data-kt-toggle-name="aside-minimize">
-							<!--begin::Svg Icon | path: icons/duotune/arrows/arr079.svg-->
-							<span class="svg-icon svg-icon-1 rotate-180">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-									<path opacity="0.5" d="M14.2657 11.4343L18.45 7.25C18.8642 6.83579 18.8642 6.16421 18.45 5.75C18.0358 5.33579 17.3642 5.33579 16.95 5.75L11.4071 11.2929C11.0166 11.6834 11.0166 12.3166 11.4071 12.7071L16.95 18.25C17.3642 18.6642 18.0358 18.6642 18.45 18.25C18.8642 17.8358 18.8642 17.1642 18.45 16.75L14.2657 12.5657C13.9533 12.2533 13.9533 11.7467 14.2657 11.4343Z" fill="currentColor" />
-									<path d="M8.2657 11.4343L12.45 7.25C12.8642 6.83579 12.8642 6.16421 12.45 5.75C12.0358 5.33579 11.3642 5.33579 10.95 5.75L5.40712 11.2929C5.01659 11.6834 5.01659 12.3166 5.40712 12.7071L10.95 18.25C11.3642 18.6642 12.0358 18.6642 12.45 18.25C12.8642 17.8358 12.8642 17.1642 12.45 16.75L8.2657 12.5657C7.95328 12.2533 7.95328 11.7467 8.2657 11.4343Z" fill="currentColor" />
-								</svg>
-							</span>
-							<!--end::Svg Icon-->
-						</div>
-						<!--end::Aside toggler-->
-					</div>
-					<!--end::Brand-->
-				</div>
-				<!--end::Aside-->
-				<!--begin::Wrapper-->
-				<div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
-					
-					<!--begin::Content-->
-					<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
-						 <!--begin::Post-->
-                         <div class="post d-flex flex-column-fluid" id="kt_post">
+    <!--begin::Main-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Page-->
+        <div class="page d-flex flex-row flex-column-fluid">
+            <!--begin::Aside-->
+            <div id="kt_aside" class="aside aside-dark aside-hoverable" data-kt-drawer="true" data-kt-drawer-name="aside" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_aside_mobile_toggle">
+                <!--begin::Brand-->
+                <div class="aside-logo flex-column-auto" id="kt_aside_logo">
+                    <!--begin::Logo-->
+                    <a href="../../demo1/dist/index.html">
+                        <img alt="Logo" src="assets/media/logos/logo-1-dark.svg" class="h-25px logo" />
+                    </a>
+                    <!--end::Logo-->
+                    <!--begin::Aside toggler-->
+                    <div id="kt_aside_toggle" class="btn btn-icon w-auto px-0 btn-active-color-primary aside-toggle" data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body" data-kt-toggle-name="aside-minimize">
+                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr079.svg-->
+                        <span class="svg-icon svg-icon-1 rotate-180">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path opacity="0.5" d="M14.2657 11.4343L18.45 7.25C18.8642 6.83579 18.8642 6.16421 18.45 5.75C18.0358 5.33579 17.3642 5.33579 16.95 5.75L11.4071 11.2929C11.0166 11.6834 11.0166 12.3166 11.4071 12.7071L16.95 18.25C17.3642 18.6642 18.0358 18.6642 18.45 18.25C18.8642 17.8358 18.8642 17.1642 18.45 16.75L14.2657 12.5657C13.9533 12.2533 13.9533 11.7467 14.2657 11.4343Z" fill="currentColor" />
+                                <path d="M8.2657 11.4343L12.45 7.25C12.8642 6.83579 12.8642 6.16421 12.45 5.75C12.0358 5.33579 11.3642 5.33579 10.95 5.75L5.40712 11.2929C5.01659 11.6834 5.01659 12.3166 5.40712 12.7071L10.95 18.25C11.3642 18.6642 12.0358 18.6642 12.45 18.25C12.8642 17.8358 12.8642 17.1642 12.45 16.75L8.2657 12.5657C7.95328 12.2533 7.95328 11.7467 8.2657 11.4343Z" fill="currentColor" />
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                    </div>
+                    <!--end::Aside toggler-->
+                </div>
+                <!--end::Brand-->
+            </div>
+            <!--end::Aside-->
+            <!--begin::Wrapper-->
+            <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+
+                <!--begin::Content-->
+                <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+                    <!--begin::Post-->
+                    <div class="post d-flex flex-column-fluid" id="kt_post">
                         <!--begin::Container-->
                         <div id="kt_content_container" class="container-xxl">
                             <!--begin::Tables Widget 13-->
@@ -71,28 +71,28 @@
                                             <!--begin::Table body-->
                                             <tbody>
                                                 <?php
-                                                    foreach($agenda as $item){
-                                                        $jambuka = date("H:i", strtotime($item->jam_buka));
-                                                        $jamtutup = date("H:i", strtotime($item->jam_tutup));
-                                                        echo '
+                                                foreach ($agenda as $item) {
+                                                    $jambuka = date("H:i", strtotime($item->jam_buka));
+                                                    $jamtutup = date("H:i", strtotime($item->jam_tutup));
+                                                    echo '
                                                         <tr>
                                                             <td class="text-dark fw-bolder text-hover-primary fs-6">
-                                                            '.$item->nama_agenda.'
+                                                            ' . $item->nama_agenda . '
                                                             </td>
                                                             <td class="text-dark fw-bolder text-hover-primary fs-6">
-                                                            '.$item->lokasi_agenda.'
+                                                            ' . $item->lokasi_agenda . '
                                                             </td>
                                                             <td class="text-dark fw-bolder text-hover-primary fs-6">
-                                                            '.$item->tgl_agenda.'
+                                                            ' . $item->tgl_agenda . '
                                                             </td>
                                                             <td class="text-dark fw-bolder text-hover-primary fs-6">
-                                                            '.$jambuka.'
+                                                            ' . $jambuka . '
                                                             </td>
                                                             <td class="text-dark fw-bolder text-hover-primary fs-6">
-                                                            '.$jamtutup.'
+                                                            ' . $jamtutup . '
                                                             </td>
                                                             <td class="text-end">
-                                                                <a href="" title="Edit Agenda" data-bs-toggle="modal" data-bs-target="#editAgenda" data-id="'.$item->id_agenda.'" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm editAgenda me-1">
+                                                                <a href="" title="Edit Agenda" data-bs-toggle="modal" data-bs-target="#editAgenda" data-id="' . $item->id_agenda . '" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm editAgenda me-1">
                                                                     <!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
                                                                     <span class="svg-icon svg-icon-3">
                                                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -102,7 +102,7 @@
                                                                     </span>
                                                                     <!--end::Svg Icon-->
                                                                 </a>
-                                                                <a href="" title="Hapus Jadwal" data-bs-toggle="modal" data-bs-target="#hapusAgenda" data-id="'.$item->id_agenda.'" class="btn btn-icon btn-bg-light btn-active-color-primary hapusAgenda btn-sm">
+                                                                <a href="" title="Hapus Jadwal" data-bs-toggle="modal" data-bs-target="#hapusAgenda" data-id="' . $item->id_agenda . '" class="btn btn-icon btn-bg-light btn-active-color-primary hapusAgenda btn-sm">
                                                                     <!--begin::Svg Icon | path: icons/duotune/general/gen027.svg-->
                                                                     <span class="svg-icon svg-icon-3">
                                                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -116,7 +116,7 @@
                                                             </td>
                                                         </tr>
                                                         ';
-                                                    }
+                                                }
                                                 ?>
                                             </tbody>
                                             <!--end::Table body-->
@@ -132,10 +132,10 @@
                         <!--end::Container-->
                     </div>
                     <!--end::Post-->
-					</div>
-					<!--end::Content-->
-					 <!--begin::Footer-->
-                     <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
+                </div>
+                <!--end::Content-->
+                <!--begin::Footer-->
+                <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
                     <!--begin::Container-->
                     <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
                         <!--begin::Copyright-->
@@ -148,26 +148,26 @@
                     <!--end::Container-->
                 </div>
                 <!--end::Footer-->
-				</div>
-				<!--end::Wrapper-->
-			</div>
-			<!--end::Page-->
-		</div>
-		<!--end::Root-->
-		
-		
-		<!--begin::Scrolltop-->
-		<div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
-			<!--begin::Svg Icon | path: icons/duotune/arrows/arr066.svg-->
-			<span class="svg-icon">
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-					<rect opacity="0.5" x="13" y="6" width="13" height="2" rx="1" transform="rotate(90 13 6)" fill="currentColor" />
-					<path d="M12.5657 8.56569L16.75 12.75C17.1642 13.1642 17.8358 13.1642 18.25 12.75C18.6642 12.3358 18.6642 11.6642 18.25 11.25L12.7071 5.70711C12.3166 5.31658 11.6834 5.31658 11.2929 5.70711L5.75 11.25C5.33579 11.6642 5.33579 12.3358 5.75 12.75C6.16421 13.1642 6.83579 13.1642 7.25 12.75L11.4343 8.56569C11.7467 8.25327 12.2533 8.25327 12.5657 8.56569Z" fill="currentColor" />
-				</svg>
-			</span>
-			<!--end::Svg Icon-->
-		</div>
-		<!--end::Scrolltop-->
+            </div>
+            <!--end::Wrapper-->
+        </div>
+        <!--end::Page-->
+    </div>
+    <!--end::Root-->
 
-	
-	</body>
+
+    <!--begin::Scrolltop-->
+    <div id="kt_scrolltop" class="scrolltop" data-kt-scrolltop="true">
+        <!--begin::Svg Icon | path: icons/duotune/arrows/arr066.svg-->
+        <span class="svg-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <rect opacity="0.5" x="13" y="6" width="13" height="2" rx="1" transform="rotate(90 13 6)" fill="currentColor" />
+                <path d="M12.5657 8.56569L16.75 12.75C17.1642 13.1642 17.8358 13.1642 18.25 12.75C18.6642 12.3358 18.6642 11.6642 18.25 11.25L12.7071 5.70711C12.3166 5.31658 11.6834 5.31658 11.2929 5.70711L5.75 11.25C5.33579 11.6642 5.33579 12.3358 5.75 12.75C6.16421 13.1642 6.83579 13.1642 7.25 12.75L11.4343 8.56569C11.7467 8.25327 12.2533 8.25327 12.5657 8.56569Z" fill="currentColor" />
+            </svg>
+        </span>
+        <!--end::Svg Icon-->
+    </div>
+    <!--end::Scrolltop-->
+    <?php $this->load->view('pmi-admin/template/footer') ?>
+
+</body>
