833515599261256b6dc258d0949192e9503c6526
Dedy Hermawan <48717251+DedyHrmawan@users.noreply.github.com>
2022-09-23 19:38:29 +0700

    add view email

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,23 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+class Welcome extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		date_default_timezone_set('Asia/Jakarta');
+	}
 
 	//authentication
 	public function VLogin()
@@ -37,11 +34,46 @@ class Welcome extends CI_Controller {
 		$this->template->depan('pmi-depan/VBlank');
 	}
 
-	//Booking Page
-	
+	public function reminderEmail()
+	{
+		$response = false;
+		$mail = new PHPMailer();
 
-	//Data Booking Admin
-	
 
-	//Laporan
+		// SMTP configuration
+		$mail->isSMTP();
+		$mail->Host     = 'smtp.googlemail.com';
+		$mail->SMTPAuth = true;
+		$mail->Username = 'uddpmimalang@gmail.com'; 
+		$mail->Password = 'pgxilesstwliahpu'; 
+		$mail->SMTPSecure = 'ssl';
+		$mail->Port     = 465;
+
+		$mail->Timeout = 60;
+		$mail->SMTPKeepAlive = true;
+
+		$mail->setFrom('uddpmimalang@gmail.com', 'UDD PMI Malang');
+		$mail->addReplyTo('uddpmimalang@gmail.com', 'UDD PMI Malang'); 
+
+		// Add a recipient
+		$mail->addAddress('deblenk.dh@gmail.com'); 
+
+		// Email subject
+		$mail->Subject = 'Reminder Donor Darah'; 
+
+		// Set email format to HTML
+		$mail->isHTML(true);
+
+		// Email body content
+		$mailContent =  $this->load->view('pmi-admin/template/email','',true);
+		$mail->Body = $mailContent;
+
+		// Send email
+		if (!$mail->send()) {
+			echo 'Message could not be sent.';
+			echo 'Mailer Error: ' . $mail->ErrorInfo;
+		} else {
+			echo 'Message has been sent';
+		}
+	}
 }
